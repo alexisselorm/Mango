bb86e387d7a151f1a85590bdcdaacdab73c9cc2a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-05-13 10:41:32 +0200

    Form to create a coupon

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -5,27 +5,32 @@ using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
-    public class CouponController : Controller
-    {
-        private readonly ICouponService _couponService;
-        private readonly ILogger<CouponController> _logger;
-        public CouponController(ICouponService couponService, ILogger<CouponController> logger)
-        {
-            _couponService = couponService;
-            _logger = logger;
-        }
-        public async Task<IActionResult> CouponIndex()
-        {
-            _logger.LogInformation("Something is wrong");
-            List<CouponDTO?> list = [];
-            var response = await _couponService.GetAllCoupons();
-            _logger.LogInformation($"{response.Message}");
-            _logger.LogInformation($"{response.Result.ToString()}");
-            if (response != null && response.IsSuccess)
-            {
-                list = JsonConvert.DeserializeObject<List<CouponDTO>>(Convert.ToString(response.Result));
-            }
-            return View(list);
-        }
-    }
+	public class CouponController : Controller
+	{
+		private readonly ICouponService _couponService;
+		private readonly ILogger<CouponController> _logger;
+		public CouponController(ICouponService couponService, ILogger<CouponController> logger)
+		{
+			_couponService = couponService;
+			_logger = logger;
+		}
+		public async Task<IActionResult> CouponIndex()
+		{
+			_logger.LogInformation("Something is wrong");
+			List<CouponDTO?> list = [];
+			var response = await _couponService.GetAllCoupons();
+			_logger.LogInformation($"{response.Message}");
+			_logger.LogInformation($"{response.Result.ToString()}");
+			if (response != null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<CouponDTO>>(Convert.ToString(response.Result));
+			}
+			return View(list);
+		}
+
+		public async Task<IActionResult> CouponCreate()
+		{
+			return View();
+		}
+	}
 }
