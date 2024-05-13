e26ac7c388b4678e03d82d04c14a4e3f61dd2803
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-05-13 19:21:54 +0200

    Custom properties to Identity user table

--- Mango.Services.AuthAPI/Data/AppDbContext.cs
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,18 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Mango.Services.AuthAPI.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.AuthAPI.Data
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        public DbSet<AppUser> AppUsers { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(builder);
         }
+
     }
 
 }
