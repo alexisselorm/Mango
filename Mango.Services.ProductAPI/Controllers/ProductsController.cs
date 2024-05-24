10aa846cb9f819c75fd4f08b880a8121e6deae60
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-05-24 20:38:19 +0200

    Added automapper and controller

---
+++ Mango.Services.ProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,140 @@
+﻿using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private ResponseDTO _response;
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+            _responseDTO = new ResponseDTO();
+        }
+
+        [HttpGet]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CouponDTO>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet("{id:int}")]
+        public ResponseDTO Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(c => c.CouponId == id);
+
+                _response.Result = _mapper.Map<CouponDTO>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpGet("GetByCode/{code}")]
+        public ResponseDTO GetByCode(string code)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(c => c.CouponCode.ToLower() == code.ToLower());
+                if (obj is null)
+                {
+                    _response.IsSuccess = false;
+                }
+                _response.Result = _mapper.Map<CouponDTO>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ADMINISTRATOR")]
+
+        public ResponseDTO Post([FromBody] CouponDTO couponDTO)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDTO);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDTO>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "ADMINISTRATOR")]
+        public ResponseDTO Put([FromBody] CouponDTO couponDTO)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDTO);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDTO>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+        [HttpDelete("{id:int}")]
+        [Authorize(Roles = "ADMINISTRATOR")]
+
+        public ResponseDTO Delete(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(c => c.CouponId == id);
+                _db.Coupons.Remove(obj);
+                _db.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+    }
+}
+}
