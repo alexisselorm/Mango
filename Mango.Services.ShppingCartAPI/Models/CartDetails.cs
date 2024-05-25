84ecf5b11d213d085ff9006784d1b4c5c2c7ec9d
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-05-25 20:47:18 +0200

    Shopping Cart API added

---
+++ Mango.Services.ShppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        [Key]
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        [ForeignKey("CartHeaderId")]
+        public CartHeader? CartHeader { get; set; }
+    }
+}
