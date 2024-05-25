d6e604252ab6a724859dd3eb5d384a0711999b51
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-05-25 02:18:58 +0200

    Loading products on page load

--- Mango.Services.ProductAPI/Controllers/ProductsController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductsController.cs
@@ -7,137 +7,136 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.ProductAPI.Controllers
 {
-    [Route("api/product")]
-    [ApiController]
-    [Authorize]
-    public class ProductsController : ControllerBase
-    {
-        private readonly AppDbContext _db;
-        private ResponseDTO _response;
-        private readonly IMapper _mapper;
-        public ProductsController(AppDbContext context, IMapper mapper)
-        {
-            _db = context;
-            _mapper = mapper;
-            _response = new ResponseDTO();
-        }
+	[Route("api/product")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly AppDbContext _db;
+		private ResponseDTO _response;
+		private readonly IMapper _mapper;
+		public ProductsController(AppDbContext context, IMapper mapper)
+		{
+			_db = context;
+			_mapper = mapper;
+			_response = new ResponseDTO();
+		}
 
-        [HttpGet]
-        public ResponseDTO Get()
-        {
-            try
-            {
-                IEnumerable<Product> objList = _db.Products.ToList();
-                _response.Result = _mapper.Map<IEnumerable<ProductDTO>>(objList);
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
-            }
-            return _response;
-        }
+		[HttpGet]
+		public ResponseDTO Get()
+		{
+			try
+			{
+				IEnumerable<Product> objList = _db.Products.ToList();
+				_response.Result = _mapper.Map<IEnumerable<ProductDTO>>(objList);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
 
-        [HttpGet("{id:int}")]
-        public ResponseDTO Get(int id)
-        {
-            try
-            {
-                Product obj = _db.Products.First(c => c.ProductId == id);
+		[HttpGet("{id:int}")]
+		public ResponseDTO Get(int id)
+		{
+			try
+			{
+				Product obj = _db.Products.First(c => c.ProductId == id);
 
-                _response.Result = _mapper.Map<ProductDTO>(obj);
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
+				_response.Result = _mapper.Map<ProductDTO>(obj);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
 
-            }
-            return _response;
-        }
+			}
+			return _response;
+		}
 
-        [HttpGet("GetByName/{name}")]
-        public ResponseDTO GetByCode(string name)
-        {
-            try
-            {
-                Product obj = _db.Products.First(c => c.Name.ToLower() == name.ToLower());
-                if (obj is null)
-                {
-                    _response.IsSuccess = false;
-                }
-                _response.Result = _mapper.Map<ProductDTO>(obj);
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
+		[HttpGet("GetByName/{name}")]
+		public ResponseDTO GetByCode(string name)
+		{
+			try
+			{
+				Product obj = _db.Products.First(c => c.Name.ToLower() == name.ToLower());
+				if (obj is null)
+				{
+					_response.IsSuccess = false;
+				}
+				_response.Result = _mapper.Map<ProductDTO>(obj);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
 
-            }
-            return _response;
-        }
+			}
+			return _response;
+		}
 
-        [HttpPost]
-        [Authorize(Roles = "ADMINISTRATOR")]
+		[HttpPost]
+		[Authorize(Roles = "ADMINISTRATOR")]
 
-        public ResponseDTO Post([FromBody] ProductDTO ProductDTO)
-        {
-            try
-            {
-                Product obj = _mapper.Map<Product>(ProductDTO);
-                _db.Products.Add(obj);
-                _db.SaveChanges();
+		public ResponseDTO Post([FromBody] ProductDTO ProductDTO)
+		{
+			try
+			{
+				Product obj = _mapper.Map<Product>(ProductDTO);
+				_db.Products.Add(obj);
+				_db.SaveChanges();
 
-                _response.Result = _mapper.Map<ProductDTO>(obj);
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
+				_response.Result = _mapper.Map<ProductDTO>(obj);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
 
-            }
-            return _response;
-        }
+			}
+			return _response;
+		}
 
-        [HttpPut]
-        [Authorize(Roles = "ADMINISTRATOR")]
-        public ResponseDTO Put([FromBody] ProductDTO ProductDTO)
-        {
-            try
-            {
-                Product obj = _mapper.Map<Product>(ProductDTO);
-                _db.Products.Update(obj);
-                _db.SaveChanges();
+		[HttpPut]
+		[Authorize(Roles = "ADMINISTRATOR")]
+		public ResponseDTO Put([FromBody] ProductDTO ProductDTO)
+		{
+			try
+			{
+				Product obj = _mapper.Map<Product>(ProductDTO);
+				_db.Products.Update(obj);
+				_db.SaveChanges();
 
-                _response.Result = _mapper.Map<ProductDTO>(obj);
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
+				_response.Result = _mapper.Map<ProductDTO>(obj);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
 
-            }
-            return _response;
-        }
-        [HttpDelete("{id:int}")]
-        [Authorize(Roles = "ADMINISTRATOR")]
+			}
+			return _response;
+		}
+		[HttpDelete("{id:int}")]
+		[Authorize(Roles = "ADMINISTRATOR")]
 
-        public ResponseDTO Delete(int id)
-        {
-            try
-            {
-                Product obj = _db.Products.First(c => c.ProductId == id);
-                _db.Products.Remove(obj);
-                _db.SaveChanges();
+		public ResponseDTO Delete(int id)
+		{
+			try
+			{
+				Product obj = _db.Products.First(c => c.ProductId == id);
+				_db.Products.Remove(obj);
+				_db.SaveChanges();
 
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
 
-            }
-            return _response;
-        }
-    }
+			}
+			return _response;
+		}
+	}
 }
--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,32 +1,47 @@
 using Mango.Web.Models;
+using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mango.Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger, IProductService productService)
+		{
+			_logger = logger;
+			_productService = productService;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public async Task<IActionResult> Index()
+		{
+			List<ProductDTO?> list = [];
+			ResponseDTO response = await _productService.GetAllProducts();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			if (response != null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(response.Result));
+			}
+			else
+			{
+				TempData["error"] = response?.Message;
+			}
+			return View(list);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
