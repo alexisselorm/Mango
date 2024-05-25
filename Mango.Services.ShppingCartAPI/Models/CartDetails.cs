132ec598a0cad016bf96e180a99e128623c4b136
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-05-25 23:17:54 +0200

    DTOs and models

--- Mango.Services.ShppingCartAPI/Models/CartDetails.cs
+++ Mango.Services.ShppingCartAPI/Models/CartDetails.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Mango.Services.ShoppingCartAPI.Models.DTO;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mango.Services.ShoppingCartAPI.Models
@@ -10,5 +11,9 @@ namespace Mango.Services.ShoppingCartAPI.Models
         public int CartHeaderId { get; set; }
         [ForeignKey("CartHeaderId")]
         public CartHeader? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        [NotMapped]
+        public ProductDTO? Product { get; set; }
+        public int Count { get; set; }
     }
 }
