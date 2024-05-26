81aafb8fbec3092125769f005f19c8084ac21f1e
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-05-26 08:18:14 +0200

    Shopping cart controller

---
+++ Mango.Services.ShppingCartAPI/Controllers/ShoppingCartAPIController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class ShoppingCartAPIController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        private ResponseDTO _response;
+        public ShoppingCartAPIController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+            _response = new ResponseDTO();
+
+        }
+
+        [HttpPost("CartUpsert")]
+        public Task<ResponseDTO> Upsert() { }
+    }
+}
