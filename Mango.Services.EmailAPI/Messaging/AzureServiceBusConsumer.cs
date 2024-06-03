8cd0bf6a7d61459a740a0d5eb4e82ef04644998a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-03 22:59:56 +0200

    Azure consumer interface and implementation

--- Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,8 +1,11 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.EmailAPI.Models.DTO;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Mango.Services.EmailAPI.Messaging
 {
-    public class AzureServiceBusConsumer
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
     {
         private readonly string serviceBusConnectionString;
         private readonly string emailCartQueue;
@@ -20,8 +23,39 @@ namespace Mango.Services.EmailAPI.Messaging
 
         }
 
+        public async Task Start()
+        {
+            _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+        }
 
+        private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
 
+            CartDTO objMessage = JsonConvert.DeserializeObject<CartDTO>(body);
+            try
+            {
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception ex)
+            {
 
+                throw;
+            }
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task Stop()
+        {
+            await _emailCartProcessor.StopProcessingAsync();
+            await _emailCartProcessor.DisposeAsync();
+        }
     }
 }
