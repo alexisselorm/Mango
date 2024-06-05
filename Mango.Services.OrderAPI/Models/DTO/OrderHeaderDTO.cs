2682aa1628752ad0708d6ebfb095a42c8c170ea5
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-05 16:10:16 +0200

    order model and dtos

---
+++ Mango.Services.OrderAPI/Models/DTO/OrderHeaderDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace Mango.Services.OrderAPI.Models.DTO
+{
+    public class OrderHeaderDTO
+    {
+        public int Order { get; set; }
+        public string UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double TotalAmount { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+
+        public DateTime OrderTime { get; set; }
+
+        public string? Status { get; set; }
+        public string? PaymentIntentId { get; set; }
+        public string? StripeSessionId { get; set; }
+        public IEnumerable<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
