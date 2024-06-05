25e6d30e9ccdaa198b93aeae78de06b2ac1610ee
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-05 16:23:08 +0200

    Scaffolding order project with files from other projects

---
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.DTO;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<OrderHeader, OrderHeaderDTO>().ReverseMap();
+                config.CreateMap<OrderDetail, OrderDetailDTO>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ Mango.Services.OrderAPI/Service/CouponService.cs
@@ -0,0 +1,32 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.DTO;
+using Mango.Services.ShoppingCartAPI.Service.IService;
+using Newtonsoft.Json;
+
+namespace Mango.Services.ShoppingCartAPI.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IHttpClientFactory _httpClient;
+        public CouponService(IHttpClientFactory httpClient)
+        {
+
+            _httpClient = httpClient;
+
+        }
+        public async Task<CouponDTO> GetCoupon(string couponCode)
+        {
+            var client = _httpClient.CreateClient("Coupon");
+            var response = await client.GetAsync($"api/Coupon/GetByCode/{couponCode}");
+            var apiContent = await response.Content.ReadAsStringAsync();
+            var resp = JsonConvert.DeserializeObject<ResponseDTO>(apiContent);
+            if (resp.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<CouponDTO>(Convert.ToString(resp.Result));
+
+            }
+            return new CouponDTO();
+        }
+
+
+    }
+}
