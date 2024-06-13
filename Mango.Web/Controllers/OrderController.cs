4b79cc7a227c52a5d9dfbf93f66c78d70adf6a87
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-13 16:49:13 +0200

    Data table

--- Mango.Web/Controllers/OrderController.cs
+++ Mango.Web/Controllers/OrderController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Mango.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+
+        }
         public IActionResult OrderIndex()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            IEnumerable<OrderHeaderDTO> list;
+            string userId = "";
+            if (!User.IsInRole(StaticDetails.RoleAdmin))
+            {
+                userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            }
+            ResponseDTO response = await _orderService.GetAllOrders(userId);
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<OrderHeaderDTO>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                list = new List<OrderHeaderDTO>();
+            }
+            return Json(new
+            {
+                data = list
+            });
         }
     }
 }
---
+++ Mango.Web/wwwroot/js/order.js
@@ -0,0 +1,11 @@
+﻿let dataTable
+function loadDataTable() {
+    dataTable = $("tblData").DataTable({
+        "ajax": { url: "/order/GetAll" },
+        "columns": [
+            {data:'orderheaderid',"width":"5%"},
+            {data:'email',"width":"25%"},
+            //{ data: 'orderheaderid', "width": 5 %},
+        ]
+    })
+}
