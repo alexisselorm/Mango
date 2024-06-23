98c82772d12d75debdfb9eb1c736ba12b4e92bbe
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-23 21:31:18 +0200

    Direct exchange rabbit MQ

--- Mango.Services.OrderAPI/RabbitMQSender/RabbitMQOrderMessageSender.cs
+++ Mango.Services.OrderAPI/RabbitMQSender/RabbitMQOrderMessageSender.cs
@@ -10,6 +10,8 @@ namespace Mango.Services.OrderAPI.RabbitMQSender
         private string _password;
         private string _hostname;
         private IConnection _connection;
+        private const string OrderCreated_RewardsUpdateQueue = "RewardsUpdateQueue";
+        private const string OrderCreated_EmailUpdateQueue = "EmailUpdateQueue";
         public RabbitMQOrderMessageSender()
         {
             _hostname = "localhost";
@@ -24,10 +26,16 @@ namespace Mango.Services.OrderAPI.RabbitMQSender
             {
 
                 using var channel = _connection.CreateModel();
-                channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, durable: false);
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Direct, durable: false);
+                channel.QueueDeclare(OrderCreated_EmailUpdateQueue, false, false, false, null);
+                channel.QueueDeclare(OrderCreated_RewardsUpdateQueue, false, false, false, null);
+
+                channel.QueueBind(OrderCreated_EmailUpdateQueue, exchangeName, "EmailUpdate");
+                channel.QueueBind(OrderCreated_RewardsUpdateQueue, exchangeName, "RewardsUpdate");
                 var json = JsonConvert.SerializeObject(message);
                 var body = Encoding.UTF8.GetBytes(json);
-                channel.BasicPublish(exchange: exchangeName, routingKey: "", null, body: body);
+                channel.BasicPublish(exchange: exchangeName, routingKey: "EmailUpdate", null, body: body);
+                channel.BasicPublish(exchange: exchangeName, routingKey: "RewardsUpdate", null, body: body);
             }
 
         }
