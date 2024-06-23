c605aeaed8eb8defdd784040c1103f8e23c6feab
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-23 14:31:37 +0200

    Rabbit MQ consumer setup

---
+++ Mango.Services.EmailAPI/Messaging/RabbitMQAuthConsumer.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Services.EmailAPI.Service;
+using RabbitMQ.Client;
+
+namespace Mango.Services.EmailAPI.Messaging
+{
+    public class RabbitMQAuthConsumer : BackgroundService
+    {
+        private IConnection _connection;
+        private IModel _channel;
+        private readonly IConfiguration _config;
+        private readonly EmailService _emailService;
+        public RabbitMQAuthConsumer(IConfiguration config, EmailService emailService)
+        {
+            _config = config;
+            _emailService = emailService;
+
+
+            var factory = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost",
+            };
+            _connection = factory.CreateConnection();
+            _channel.QueueDeclare(_config["TopicAndQueueNames:RegisterUserQueue"], false, false, false, null);
+
+
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
